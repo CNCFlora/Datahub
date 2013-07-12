d3dbb9b536e572e24eedf620673a68d0dfcc9064
Diogo Souza <diogo@diogok.net>
2013-07-12 17:38:45 -0300

    emitting only species, subspecies and variety on species_by_family view

--- taxonomy/views/species_by_family/map.js
+++ taxonomy/views/species_by_family/map.js
@@ -1,7 +1,9 @@
 function(doc) {
     if(doc.metadata.type =='taxon') {
-        if(doc.taxonomicStatus == "accepted") {
-            emit(doc.family.toUpperCase(),doc);
+        if(doc.taxonomicStatus == "accepted" ) {
+            if(doc.taxonRank == 'species' || doc.taxonRank == 'subspecies' || doc.taxonRank == 'variety') {
+                emit(doc.family.toUpperCase(),doc);
+            }
         }
     }
 }
