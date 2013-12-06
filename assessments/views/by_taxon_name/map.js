934df029b1b9fd047537aa326d6f025ea088bb8a
Diogo Souza <diogo@diogok.net>
2013-12-06 10:44:58 -0200

    fixed assessment by taxon name view

--- assessments/views/by_taxon_name/map.js
+++ assessments/views/by_taxon_name/map.js
@@ -1,5 +1,5 @@
 function(doc) {
-    if(doc.metadata.type == 'assessment' && doc.status == 'published') {
+    if(doc.metadata.type == 'assessment' && doc.metadata.status == 'published') {
         emit(doc.taxon.scientificName,doc);
     }
 }
