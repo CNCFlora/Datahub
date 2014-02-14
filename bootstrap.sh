18625ed415885369cffad91d46dbd4dc71efa5d4
Diogo Souza <diogo@diogok.net>
2014-02-14 14:21:30 -0200

    fix vagrant bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,7 @@ aptitude install git curl tmux couchdb libgd2-noxpm -y
 # setup couchdb
 sed -i -e 's/;bind_address.*/bind_address = 0.0.0.0/' /etc/couchdb/local.ini 
 sed -i -e 's/;port/port/' /etc/couchdb/local.ini 
+service couchdb restart
 curl -X PUT "http://localhost:5984/cncflora"
 curl -X PUT "http://localhost:5984/cncflora_test"
 
@@ -14,7 +15,6 @@ curl -X PUT "http://localhost:5984/cncflora_test"
 echo 'echo $(date) > /var/log/rc.log' > /etc/rc.log
 echo "service couchdb start >> /var/log/rc.log 2>&1 " >> /etc/rc.local
 echo 'SUBSYSTEM=="bdi",ACTION=="add",RUN+="/vagrant/register.sh >> /var/log/rc.log 2>&1"' > /etc/udev/rules.d/50-vagrant.rules
-service couchdb start
 
 # install the datahub design docs
 cd /vagrant
